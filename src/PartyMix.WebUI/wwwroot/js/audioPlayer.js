9240f38b418450a7ae170ea986a7fb79a4574edc
sciapan <sciapan.r@gmail.com>
2024-11-09 21:10:48 +0100

    Add music player capabilities

---
+++ src/PartyMix.WebUI/wwwroot/js/audioPlayer.js
@@ -0,0 +1,38 @@
+﻿window.audioPlayer = {
+    audio: null,
+    dotNetReference: null,
+
+    initialize: function (dotNetRef) {
+        this.dotNetReference = dotNetRef;
+        this.audio = new Audio();
+
+        this.audio.addEventListener('timeupdate', () => {
+            const percentage = (this.audio.currentTime / this.audio.duration) * 100;
+            this.dotNetReference.invokeMethodAsync('OnPlaybackProgress', percentage);
+        });
+
+        this.audio.addEventListener('ended', () => {
+            this.dotNetReference.invokeMethodAsync('OnSongEnded');
+        });
+    },
+
+    loadAndPlay: function (url) {
+        if (this.audio) {
+            this.audio.src = url;
+            this.audio.load();
+            this.audio.play();
+        }
+    },
+
+    play: function () {
+        if (this.audio) {
+            this.audio.play();
+        }
+    },
+
+    pause: function () {
+        if (this.audio) {
+            this.audio.pause();
+        }
+    }
+};
